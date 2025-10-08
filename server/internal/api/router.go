0fff563e9d81f67895df82331a89034b7247cc1d
Carl Thibault <lalogiquefloue@gmail.com>
2025-10-08 14:37:41 -0400

    refactor API routes and serve webui

--- server/internal/api/router.go
+++ server/internal/api/router.go
@@ -4,13 +4,22 @@ import (
 	"server/internal/api/handlers"
 	"server/internal/configs"
 
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine, config *configs.Config) {
 	h := handlers.NewHandlers(config)
 
-	router.GET("/", h.Home)
-	router.POST("/sensor/data", h.SensorData)
-	router.GET("/sensor/latest", h.GetLatestSensorData)
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./webui/dist", true)))
+
+	// API Endpoints
+	api := router.Group("/api")
+	{
+		api.GET("/status", h.Home)
+		api.GET("/sensor/latest", h.GetLatestSensorData)
+		api.POST("/sensor/data", h.SensorData)
+	}
+
 }

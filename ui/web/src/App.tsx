fd21cd95ee03d257d79e3200feffe9a861e8ef02
Carl Thibault <lalogiquefloue@gmail.com>
2025-10-08 10:52:44 -0400

    add basic web ui for realtime data

---
+++ ui/web/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Dashboard from './components/Dashboard.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
---
+++ ui/web/src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import RealtimeDataTable from "./Dashboard/RealtimeDataTable"
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1>ESP32 Climate Dashboard</h1>
+        <RealtimeDataTable />
+    </div>
+  )
+}
+
+export default Dashboard

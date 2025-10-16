d03c9b3d84857a1d76977e8203f08f03a17ab276
Carl Thibault <lalogiquefloue@gmail.com>
2025-10-16 14:19:55 -0400

    add data chart generation

--- server/webui/src/components/Dashboard.tsx
+++ server/webui/src/components/Dashboard.tsx
@@ -3,10 +3,12 @@ import RealtimeDataTable from "./Dashboard/RealtimeDataTable"
 import DataChart from "./Dashboard/DataChart"
 
 const Dashboard = () => {
-  const [range, setRange] = useState("1h");
+  const [range, setRange] = useState("3h");
+  // const [field, setField] = useState<"temperature" | "humidity">("temperature");
   // const [sensorID, setSensorID] = useState("");
 
-  const ranges = ["5m", "15m", "30m", "1h", "6h", "12h", "24h", "7d", "30d", "365d"];
+  const ranges = ["5m", "15m", "30m", "1h", "3h", "6h", "12h", "24h", "7d", "30d", "365d"];
+  // const fields = ["temperature", "humidity"];
   // const sensorOptions = [] // TODO: implement sensorID selection, fetch available sensors
 
   return (
@@ -15,17 +17,15 @@ const Dashboard = () => {
           <h1>ESP32 Climate Dashboard</h1>
           <RealtimeDataTable />
         </div>
-
         <br />
-
         <div>
           <h2>Historical Data</h2>
           <label htmlFor="range">Select Range: </label>
           <select value={range} onChange={(e) => setRange(e.target.value)}>
             {ranges.map((r) => (<option key={r} value={r}>{r}</option>))}
           </select>
-
-          <DataChart range={range} /> 
+          <DataChart range={range} field="temperature" /> 
+          <DataChart range={range} field="humidity" /> 
         </div>
     </div>
   )

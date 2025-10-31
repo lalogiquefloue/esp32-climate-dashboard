731c3d26679fcaca0008f6e89d278fd06641a7ad
Carl Thibault <lalogiquefloue@gmail.com>
2025-10-31 11:05:19 -0400

    deactivate local dev URL

--- server/webui/src/components/Dashboard/RealtimeData.tsx
+++ server/webui/src/components/Dashboard/RealtimeData.tsx
@@ -14,8 +14,8 @@ const RealtimeData = () => {
 
   useEffect(() => {
     const fetchData = () => {
-      fetch("http://localhost:8765/api/sensor/latest") // for local dev
-        // fetch("/api/sensor/latest")
+      // fetch("http://localhost:8765/api/sensor/latest") // for local dev
+        fetch("/api/sensor/latest")
         .then(response => {
           if (!response.ok) throw new Error("Server error...")
           return response.json()
